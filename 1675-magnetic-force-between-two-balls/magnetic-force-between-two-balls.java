110151a1d7310c3d504ddeb5e3513211410cc3a4
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-07-21 20:48:57 +0530

    Time: 47 ms (48.81%) | Memory: 79 MB (83.76%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -1,12 +1,14 @@
 class Solution {
     public int maxDistance(int[] position, int m) {
         Arrays.sort(position);
-        int n = position.length;
+        int n  = position.length;
         int low=1;
         int high=position[n-1]-position[0];
+        // int ans=0;
         while(low<=high){
             int mid = low+(high-low)/2;
-            if(maxamongminforce(position,mid,m)){
+            if(miniforce(position,mid,m)){
+                // ans=mid;
                 low=mid+1;
             }else{
                 high=mid-1;
@@ -14,13 +16,13 @@ class Solution {
         }
         return high;
     }
-    public static boolean maxamongminforce(int[] arr , int mid , int m){
+    public boolean miniforce(int[] position , int mid , int m){
+        int lastposition = position[0];
         int balls=1;
-        int lastposition=arr[0];
-        for(int i=1;i<arr.length;i++){
-            if((arr[i]-lastposition)>=mid){
+        for(int i=1;i<position.length;i++){
+            if((position[i]-lastposition)>=mid){
                 balls++;
-                lastposition=arr[i];
+                lastposition=position[i];
             }
         }
         return balls>=m;
