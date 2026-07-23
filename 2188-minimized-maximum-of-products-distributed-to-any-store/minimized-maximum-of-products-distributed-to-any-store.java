86aa829094c8ee77bf470ca13d8d3b50b4559693
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-07-23 21:54:32 +0530

    Time: 23 ms (89.11%) | Memory: 81.1 MB (51.62%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int m = quantities.length;
+        int low =1;
+        int high=0;
+        for(int i : quantities){
+            high=Math.max(high,i);
+        }
+        int ans=0;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(minofmaxproducts(quantities,mid,n)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public boolean minofmaxproducts(int[] quantities , int mid , int n){
+        long distribution = 0;
+        for(int i : quantities){
+            distribution += (i+mid-1)/mid;
+        }
+        return distribution<=n;
+    }
+}
