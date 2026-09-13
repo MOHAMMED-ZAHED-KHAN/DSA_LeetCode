3c4cd68a7564892797ea2f916cd3ca8d81ae49ea
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-09-13 19:49:07 +0530

    Time: 1 ms (96.08%) | Memory: 47.6 MB (42.14%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,8 +2,7 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        boolean firstrowhas0=false;        
-        boolean firstcolhas0=false;
+        boolean firstrowhas0=false , firstcolhas0=false;
         for(int i=0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(matrix[i][j]==0){
