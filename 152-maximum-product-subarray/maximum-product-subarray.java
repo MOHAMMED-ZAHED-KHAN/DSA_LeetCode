2e1c51389cfcad9dd41ea0672c78b5d4f9407a5a
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2026-09-20 16:26:05 +0530

    Time: 1 ms (93.28%) | Memory: 47.6 MB (15.79%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int prefixproduct=1;
+        int suffixproduct=1;
+        int maxproduct=nums[0];
+        for(int i=0;i<n;i++){
+            if(prefixproduct==0){
+                prefixproduct=1;
+            }
+            if(suffixproduct==0){
+                suffixproduct=1;
+            }
+            prefixproduct*=nums[i];
+            suffixproduct*=nums[n-i-1];
+            maxproduct=Math.max(maxproduct,Math.max(prefixproduct,suffixproduct));
+        }
+        return maxproduct;
+    }
+}
